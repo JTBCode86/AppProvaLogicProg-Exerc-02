32276d0e37c12bdd953c5b1908fab493d29298bf
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-06 22:12:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AppProvaLogicProg_Exerc_02
+{
+    class Program
+    {
+        public static double CalcularValorAreaCirculo(double raio) 
+        {
+            double area;
+            double Pi = 3.14159;
+            area = Pi * Math.Pow(raio, 2); 
+            return area;
+        }
+
+        static void Main(string[] args)
+        {
+            double raio;
+            double valorArea;
+            Console.WriteLine("Entre com o valor do raio:");
+            raio = double.Parse(Console.ReadLine());
+            valorArea = CalcularValorAreaCirculo(raio);
+
+            Console.WriteLine($"O valor da area do circulo é: {valorArea:00.0000}");
+            Console.ReadLine();
+        }
+    }
+}
